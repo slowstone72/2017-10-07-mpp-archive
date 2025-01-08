698f7918729476b5b2b3a8e3c7ab4393681a237b
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-08 19:39:59 +0000

    Create MPPAnnouncerShutdown.js

---
+++ JavaScript/MPPAnnouncerShutdown.js
@@ -0,0 +1,11 @@
+﻿/* Ancient code published for archival purposes. May or may not work or be relevant.
+Feel free to do whatever you want with this, if anything.
+Archive from 2017-10-07 - contents may be older. */
+
+setTimeout(function(){ MPP.chat.send('Hm, it looks like the system has been active for a while now.'); }, 1000);      
+setTimeout(function(){ MPP.chat.send(': ' + counter1 + ' people have joined and ' + counter2 + ' people have left.'); }, 2000); 
+setTimeout(function(){ MPP.chat.send('It is now: ' + Date() + '.'); }, 30000);      
+setTimeout(function(){ MPP.chat.send('Deactivating..'); }, 4000); 
+setTimeout(function(){ MPP.chat.send('Deactivating...'); }, 8000); 
+setTimeout(function(){ MPP.chat.send('_'); }, 10000); 
+setTimeout(function(){ location.reload() }, 11000);  
